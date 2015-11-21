ff2b6559cbd2fe22440f38f465f30c535ae04d5b
Brad Smith <brad@compasshb.com>
2015-11-21 01:51:11 -0800

    validate podcast feed

--- resources/views/podcasts/video.blade.php
+++ resources/views/podcasts/video.blade.php
@@ -1,6 +1,6 @@
 {!! '<?xml version = "1.0" encoding = "utf-8"?>' !!}
 
-<rss xmlns:itunes="https://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 	<channel>
 		<title>Compass HB Sermons</title>
 		<itunes:subtitle>The gospel rings out...</itunes:subtitle>
@@ -15,13 +15,14 @@
 			<itunes:category text="Christianity"/>
 		</itunes:category>
 		<itunes:category text="Education"/>
+		<atom:link href="https://www.compasshb.com/feed/sermons" rel="self" type="application/rss+xml" />
 
 		@foreach ($sermons as $sermon)
 			<item>
 				<title>{{ $sermon->title }}</title>
 				<link>{{ route('sermons.show', $sermon->slug) }}</link>
 				<itunes:subtitle>{{ $sermon->byline }} {{ $sermon->text }}</itunes:subtitle>
-				<enclosure url="{{ URL::to('/') }}/sermons/{{ $sermon->slug }}/download" length="160000000" type="audio/x-mp4" />
+				<enclosure url="{{ URL::to('/') }}/sermons/{{ $sermon->slug }}/download/" length="160000000" type="audio/x-mp4" />
 				<description>{{ $sermon->byline }} {{ $sermon->text }}</description>
 				<pubDate>{{ $sermon->published_at->toRfc2822String() }}</pubDate>
 				<guid>{{ route('sermons.show', $sermon->slug) }}</guid>
