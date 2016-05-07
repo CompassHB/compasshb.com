CompassHB/compasshb.com

commit 24fbca80c3f5dc5d87fbd070d4f87c307929ef6b

change column names

app/Series.php
@@ -26,7 +26,7 @@ class Series extends Model
      */
     public function setMinistryAttribute($value)
     {
-        $this->attributes['ministry'] = $value ? $value : null;
+        $this->attributes['ministryId'] = $value ? $value : null;
     }
 
     /**

app/Console/Commands/WorksheetReminder.php
@@ -37,7 +37,7 @@ class WorksheetReminder extends Command
      */
     public function handle()
     {
-        $sermon = Sermon::where('ministry', '=', null)->latest('published_at')->published()->get()->first();
+        $sermon = Sermon::where('ministryId', '=', null)->latest('published_at')->published()->get()->first();
 
         if ($sermon->worksheet == null) {
             Mail::send('emails.worksheet-reminder', [], function ($message) {

app/Http/Controllers/Api/APIController.php
@@ -12,7 +12,7 @@ class APIController extends Controller
 {
     public function index(Video $video)
     {
-        $sermons = Sermon::where('ministry', '=', null)->latest('published_at')->published()->get();
+        $sermons = Sermon::where('ministryId', '=', null)->latest('published_at')->published()->get();
 
         foreach ($sermons as $sermon) {
             $video->setUrl($sermon->video);

app/Http/Controllers/Api/SermonsController.php
@@ -15,7 +15,7 @@ class SermonsController extends Controller
      */
     public function index()
     {
-        $sermons = Sermon::where('ministry', '=', null)->latest('published_at')->published()->get();
+        $sermons = Sermon::where('ministryId', '=', null)->latest('published_at')->published()->get();
 
         return Response::json($sermons->toArray());
     }

app/Http/Controllers/FeedsController.php
@@ -28,7 +28,7 @@ class FeedsController extends Controller
     public function sermons()
     {
         $data = array();
-        $data['sermons'] = Sermon::where('ministry', '=', null)
+        $data['sermons'] = Sermon::where('ministryId', '=', null)
                                 ->published()
                                 ->orderBy('published_at', 'desc')
                                 ->limit(300)
@@ -46,7 +46,7 @@ class FeedsController extends Controller
      */
     public function sermonaudio()
     {
-        $data['sermons'] = Sermon::where('ministry', '=', null)->published()->orderBy('published_at', 'desc')->limit(300)->get();
+        $data['sermons'] = Sermon::where('ministryId', '=', null)->published()->orderBy('published_at', 'desc')->limit(300)->get();
 
         return Response::view('podcasts.audio', $data, 200, [
             'Content-Type' => 'application/atom+xml; charset=UTF-8',
@@ -62,7 +62,7 @@ class FeedsController extends Controller
     public function json(Video $video)
     {
         $data = array();
-        $data['sermons'] = Sermon::where('ministry', '=', null)->orderBy('published_at', 'desc')->published()->limit(300)->get();
+        $data['sermons'] = Sermon::where('ministryId', '=', null)->orderBy('published_at', 'desc')->published()->limit(300)->get();
 
         // Retrieve coverart
         foreach ($data['sermons'] as $sermon) {

app/Http/Controllers/FellowshipsController.php
@@ -17,7 +17,7 @@ class FellowshipsController extends Controller
      */
     public function index(Video $video, Events $event)
     {
-        $sermon = Sermon::where('ministry', '=', null)->latest('published_at')->published()->take(1)->get()->first();
+        $sermon = Sermon::where('ministryId', '=', null)->latest('published_at')->published()->take(1)->get()->first();
 
         $video->setUrl($sermon->video);
         $sermon->iframe = $video->getEmbedCode();

app/Http/Controllers/HomeController.php
@@ -49,8 +49,8 @@ class HomeController extends Controller
 
     public function mainservice()
     {
-        $sermons = Sermon::where('ministry', '=', null)->latest('published_at')->paginate(15);
-        $series = Series::where('ministry', '=', null)->paginate(15);
+        $sermons = Sermon::where('ministryId', '=', null)->latest('published_at')->paginate(15);
+        $series = Series::where('ministryId', '=', null)->paginate(15);
         $blogs = Blog::latest('published_at')->paginate(15);
 
         return view('admin.mainservice', compact('sermons', 'blogs', 'series'))
@@ -79,8 +79,8 @@ class HomeController extends Controller
      */
     public function video()
     {
-        $sermons = Sermon::whereNotNull('ministry')->latest('published_at')->paginate(15);
-        $series = Series::whereNotNull('ministry')->paginate(15);
+        $sermons = Sermon::whereNotNull('ministryId')->latest('published_at')->paginate(15);
+        $series = Series::whereNotNull('ministryId')->paginate(15);
 
         return view('admin.video', compact('sermons', 'series'))
             ->with('title', 'Admin - Videos');

app/Http/Controllers/MinistryController.php
@@ -27,8 +27,8 @@ class MinistryController extends Controller
      */
     public function youth(Video $videoClient)
     {
-        $series = Series::where('ministry', '=', 'youth')->get()->reverse();
-        $sermons = Sermon::where('ministry', '=', 'youth')->latest('published_at')->published()->get();
+        $series = Series::where('ministryId', '=', 'youth')->get()->reverse();
+        $sermons = Sermon::where('ministryId', '=', 'youth')->latest('published_at')->published()->get();
         $prevsermon = $sermons->first();
 
         if ($prevsermon) {
@@ -58,8 +58,8 @@ class MinistryController extends Controller
      */
     public function sundayschool()
     {
-        $series = Series::where('ministry', '=', 'sundayschool')->get()->reverse();
-        $sermons = Sermon::where('ministry', '=', 'sundayschool')
+        $series = Series::where('ministryId', '=', 'sundayschool')->get()->reverse();
+        $sermons = Sermon::where('ministryId', '=', 'sundayschool')
             ->where('series_id', '=', $series->first()->id)
             ->latest('published_at')
             ->published()
@@ -79,7 +79,7 @@ class MinistryController extends Controller
      */
     public function women(Video $video)
     {
-        $sermons = Sermon::where('ministry', '=', 'women')->latest('published_at')->published()->get();
+        $sermons = Sermon::where('ministryId', '=', 'women')->latest('published_at')->published()->get();
 
         foreach ($sermons as $sermon) {
             $video->setUrl($sermon->video);

commit 3679ed294ac45dce5b83a9b34a253e78717b0059

Fix inconsistency introduced by 24fbca80c3f5dc5d87fbd070d4f87c307929ef6b

app/Series.php
@@ -13,7 +13,7 @@ class Series extends Model
         'title',
         'body',
         'image',
-        'ministry',
+        'ministryId',
     ];
 
     protected $dates = ['published_at', 'deleted_at'];