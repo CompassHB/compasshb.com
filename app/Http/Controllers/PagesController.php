a24eb31ae5d8652ae42b1441015623b8fcba3f62
Brad Smith <bradmsmith@live.com>
2016-06-07 12:21:03 -0700

    debug events

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -242,11 +242,6 @@ class PagesController extends Controller
             // All Events
             $events = $event->events();
 
-            // Filter out Home Fellowship Group events
-            $events = array_filter($events, function ($var) {
-                return ($var->organizer_id != '8215662871');
-            });
-			
 			dd($events);
 
             // Events accepting registrations
