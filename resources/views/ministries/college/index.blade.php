3ef08f46e15c66e48ccc7b9a0f1ccbcc99563008
Brad Smith <bradmsmith@users.noreply.github.com>
2016-06-08 06:21:00 -0700

    fix typo in email

--- resources/views/ministries/college/index.blade.php
+++ resources/views/ministries/college/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <h1 class="tk-seravek-web">The Underground College Ministry</h1>
-    <p>The college ministry meets Sundays at 7PM for dinner, teaching and small groups at the church. For more information contact info@compashb.com</p>
+    <p>The college ministry meets Sundays at 7PM for dinner, teaching and small groups at the church. For more information contact info@compasshb.com</p>
     <p><img src="https://scontent-iad3-1.xx.fbcdn.net/hphotos-xfa1/t31.0-8/12244626_782033515239161_5841761662630853834_o.jpg" style="width: 400px"/></p>
     <br/><br/><br/>
 @endsection
