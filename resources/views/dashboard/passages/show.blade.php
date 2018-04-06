2793a3cc9d743842dc35cf8c791b5ea4a8863887
Brad Smith <bradmsmith@users.noreply.github.com>
2018-04-06 09:01:09 -0700

    pray a psalm a day

--- resources/views/dashboard/passages/show.blade.php
+++ resources/views/dashboard/passages/show.blade.php
@@ -9,6 +9,9 @@
 <link rel="canonical" href="{{ route('read.show', $passage->slug) }}/" />
 
   <p>{!! $postflash !!}</p>
+  
+        <h4>Pray a Psalm a Day Videos</h4>
+      <p>Watch the <a href="https://vimeo.com/channels/1364252">videos for Pray Psalm a Day here</a>. Uploaded daily.</p>
 
   <div class="Setting Box Box--Large Box--bright utility-flex">
     <h1 class="Setting__heading tk-seravek-web">{{ $passage->title->rendered }}</h1>
@@ -30,6 +33,9 @@
           <li><a href="{{ route('read.show', $p->slug) }}">{{ $p->title->rendered }}</a></li>
         @endforeach
         </ul>
+        
+      <h4>Pray a Psalm a Day Videos</h4>
+      <p>Watch the <a href="https://vimeo.com/channels/1364252">videos for Pray Psalm a Day here</a>. Uploaded daily.</p>
 
     	@include('dashboard.passages.comments')
 
